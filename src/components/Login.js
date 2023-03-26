8ca518e2c588ecc0d66c8125e68a913083848381
kingkazuma7 <your.nasionasio7@gmail.com>
2023-03-26 11:02:24 +0900

    32

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginInWidthGoogle = () => {
+    // google login
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  }
+  
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWidthGoogle}>Googleでログイン</button>
+    </div>
   )
 }
 
