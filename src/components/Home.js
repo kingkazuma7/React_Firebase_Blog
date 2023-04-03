21caf50dbbd8452fd9dc21ebb9ceac05b273b3a2
kingkazuma7 <your.nasionasio7@gmail.com>
2023-04-03 10:08:49 +0900

    42

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 import "./Home.scss";
 
 function Home() {
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      console.log(data.docs.map((doc) => ({ ...doc.data() })));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
       <div className="postContents">
