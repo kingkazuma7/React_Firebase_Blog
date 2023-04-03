bdd56a51538a0b2a49ab9ae5e83873638e5333a1
kingkazuma7 <your.nasionasio7@gmail.com>
2023-04-03 09:41:45 +0900

    39

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
 import "./CreatePost.scss";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
   const [title, setTitle] = useState();
   const [postText, setPostText] = useState();
-  const createPost = () => {
-    console.log(title);
-    console.log(postText);
+  
+  const navigate = useNavigate();
+  
+  const createPost = async () => {
+    if (!auth.currentUser) {
+      // ユーザーがログインしていない場合の処理
+      console.log("ユーザーがログインしていない場合の処理");
+      return;
+    }
+    await addDoc(collection(db, "posts"), {
+      title: title,
+      postText: postText,
+      author: {
+        username: auth.currentUser.displayName,
+        id: auth.currentUser.uid,
+      }
+    });
+    
+    navigate("/");
   };
 
   return (
