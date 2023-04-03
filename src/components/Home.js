26c69d2e9fc58d63a1a77dbd19fd8f1c20cdf267
kingkazuma7 <your.nasionasio7@gmail.com>
2023-04-03 09:50:23 +0900

    41 add

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./Home.scss";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はreact学習中です。ほげほげほげほげ。
+        </div>
+        <div className="nameDeleteButton">
+          <h3>@nasio</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
